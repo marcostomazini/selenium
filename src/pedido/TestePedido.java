bc493690e4fda52c9fe8c4402e4954a56a8813c1
marcos.tomazini@gmail.com <marcos.tomazini@gmail.com>
2015-08-26 17:45:57 +0000

    try finally

--- src/pedido/TestePedido.java
+++ src/pedido/TestePedido.java
@@ -1,12 +1,15 @@
 package pedido;
 
+import org.junit.internal.runners.statements.Fail;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -16,6 +19,7 @@ public class TestePedido {
     @Test
     public void test() {
         WebDriver driver = new FirefoxDriver();
+        try {
         driver.get("http://planned.by/quickloja");
         driver.manage().window().maximize();
 
@@ -46,6 +50,27 @@ public class TestePedido {
 
         driver.findElement(By.id("adicionar")).click();
 
-        //driver.quit();
+
+        driver.findElement(By.id("produtonome")).sendKeys("bon");
+
+        //Camisa Gant The Banker Rosa
+        await.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("li[data-value='Boné Quiksilver Hitchi Hike Cinza']")));
+
+        driver.findElement(By.cssSelector("li[data-value='Boné Quiksilver Hitchi Hike Cinza']")).click();
+
+        driver.findElement(By.id("produtoquantidade")).sendKeys("2");
+
+        driver.findElement(By.id("adicionar")).click();
+
+        Assert.assertEquals("Camisa Gant The Banker Rosa",
+                driver.findElement(By.xpath("//tr[1]/td[2]")).getText());
+        } catch (Exception e) {
+            //TakesScreenshot
+        } catch (AssertionError e) {
+            //fail();
+        } finally {
+            driver.quit();
+        }
     }
 }
